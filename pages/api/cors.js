7d2bee7b4a0f2ea61a236560887285f88b5b3f9b
Christopher de Beer <christopherdebeer@users.noreply.github.com>
2024-08-31 23:11:21 +0100

    Update cors.js

--- pages/api/cors.js
+++ pages/api/cors.js
@@ -1,5 +1,5 @@
 module.exports = function(req,res) {
-  res.header('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 }
