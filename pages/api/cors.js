6123a2525c1ee7c4246be64775d04e18a17863f1
Christopher de Beer <christopherdebeer@users.noreply.github.com>
2024-08-31 22:59:58 +0100

    Create cors.js

---
+++ pages/api/cors.js
@@ -0,0 +1,5 @@
+export default function(req,res) {
+  res.header('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+}
